4d694cd4fa3e8d31145c2d2b1cc4cb7642cd0540
toert <pw177@mail.ru>
2017-10-09 12:59:40 +0300

    Изменения моделей с учетом имеющихся данных

--- universities/models.py
+++ universities/models.py
@@ -2,46 +2,92 @@ from django.db import models
 
 
 class University(models.Model):
+    TYPE_GOVERNMENT = 'gov'
+    TYPE_MUNICIPAL = 'mun'
+    TYPE_PRIVATE = 'ind'
+
+    TYPES = (
+        (TYPE_GOVERNMENT, 'Государственный'),
+        (TYPE_MUNICIPAL, 'Муниципальный'),
+        (TYPE_PRIVATE, 'Частный'),
+    )
+
     name = models.CharField(max_length=255)
     year = models.PositiveIntegerField()
     employment_percentage = models.FloatField()
     average_salary = models.FloatField()
     has_military_department = models.BooleanField(default=False)
     has_dormitory = models.BooleanField(default=False)
-
-    def __str__(self):
-        return '{}'.format(self.name)
-
-
-class Faculty(models.Model):
-    university = models.ForeignKey(University, related_name='faculties')
-    name = models.CharField(max_length=255)
+    region_name = models.CharField(max_length=255)
+    type = models.CharField(max_length=3, choices=TYPES)
 
     def __str__(self):
         return '{}'.format(self.name)
 
 
 class Program(models.Model):
-    faculty = models.ForeignKey(Faculty, related_name='programs')
-    year = models.PositiveIntegerField()
-    # Postuplenie
-    name = models.CharField(max_length=255)
-    code = models.CharField(max_length=255)
-    first_passing_score = models.PositiveIntegerField()
-    second_passing_score = models.PositiveIntegerField(blank=True, null=True)
-    # Graduate
+    FORM_FULLTIME = 'fulltime'
+    FORM_DISTANCE = 'distance'
+    FORM_EXTRAMURAL = 'extramural'
+    FORM_MIXED = 'mixed'
+    FORM_NIGHT = 'night'
+
+    FORMS = ((FORM_FULLTIME,'Очная'),
+             (FORM_DISTANCE, 'Дистанционная'),
+             (FORM_EXTRAMURAL, 'Заочная'),
+             (FORM_MIXED, 'Очно-заочная'),
+             (FORM_NIGHT, 'Вечерняя'))
+
+    LEVEL_BACHELOR = 'bachelor'
+    LEVEL_SPECIALTY = 'specialty'
+
+    LEVELS = ((LEVEL_BACHELOR, 'Бакалавриат'),
+              (LEVEL_SPECIALTY, 'Специалитет'))
+
+    university = models.ForeignKey(University, related_name='programs')
     average_salary = models.FloatField()
+    code = models.CharField(max_length=255)
+    common_name = models.CharField(max_length=255)
+    cost_per_year = models.PositiveIntegerField()
+    custom_exam = models.CharField(max_length=255, blank=True)
+    duration = models.PositiveIntegerField()
     employment_percentage = models.FloatField()
-    average_age = models.PositiveIntegerField()
-
-
-class Exam(models.Model):
-    name = models.CharField(max_length=255)
-    minimal_score = models.IntegerField()
+    first_passing_score = models.PositiveIntegerField()
+    full_name = models.CharField(max_length=255)
+    form = models.CharField(max_length=64, choices=FORMS)
+    level = models.CharField(max_length=255)
+    second_passing_score = models.PositiveIntegerField(blank=True, null=True)
+    places = models.PositiveIntegerField()
+    ucheba_url = models.CharField(max_length=255)
     year = models.PositiveIntegerField()
 
 
 class RequiredExam(models.Model):
+    EXAM_SOCIAL_STUDIES = 'soc'
+    EXAM_GEOGRAPHY = 'geo'
+    EXAM_LITERATURE = 'lit'
+    EXAM_PHYSICS = 'phy'
+    EXAM_HISTORY = 'his'
+    EXAM_FOREIGN = 'for'
+    EXAM_ICT = 'ict'
+    EXAM_RUSSIAN = 'rus'
+    EXAM_MATH = 'mat'
+    EXAM_CHEMISTRY = 'che'
+    EXAM_BIOLOGY = 'bio'
+
+    EXAMS = ((EXAM_SOCIAL_STUDIES, 'Обществознание'),
+             (EXAM_GEOGRAPHY, 'География'),
+             (EXAM_LITERATURE, 'Литература'),
+             (EXAM_PHYSICS, 'Физика'),
+             (EXAM_HISTORY, 'История'),
+             # ('eng', 'Английский язык'), !!! TODO ADD English as Foreign language
+             (EXAM_FOREIGN, 'Иностранный язык'),
+             (EXAM_ICT, 'Информатика и ИКТ'),
+             (EXAM_RUSSIAN, 'Русский язык'),
+             (EXAM_MATH, 'Математика'),
+             (EXAM_CHEMISTRY, 'Химия'),
+             (EXAM_BIOLOGY, 'Биология'))
+
     program = models.ForeignKey(Program, related_name='exams')
-    exam = models.ForeignKey(Exam, related_name='programs')
+    exam = models.CharField(max_length=3, choices=EXAMS)
 
