85a178201e874837c7e77dd278b2df3f4f21085b
toert <pw177@mail.ru>
2017-10-23 14:19:23 +0300

    Use AJAX to send an user-generated request

--- calculator/views.py
+++ calculator/views.py
@@ -1,54 +1,46 @@
 from django.shortcuts import render
 from django.db.models import F
 from django.views.generic.detail import DetailView
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.views.generic import ListView, FormView
 from calculator.forms import ExamsForm
-from universities.models import RequiredExam, Program
-
-# ExamFormSet = formset_factory(ExamForm, extra=len(RequiredExam.EXAMS))
-# some_formset = ExamFormSet(label=[{'id': 'x.id'} for x in some_objects])
+from universities.models import University, Program
 
 
-def main_page(request):
-    if request.method == 'GET':
-        return render(request, 'calculator/calculator_page.html',
-                      {'score_form': ExamsForm})
-    elif request.method == 'POST':
-        form = ExamsForm(request.POST)
-        if form.is_valid():
-            form_data = form.cleaned_data
-            suitable_programs = Program.objects.prefetch_related('exams', 'university')
-            if form_data['city'] != 'all':
-                suitable_programs = suitable_programs.filter(university__region_name=form_data['city'])
-            if form_data['sort_by'] == 'salary':
-                suitable_programs = suitable_programs.order_by(F('average_salary').desc(nulls_last=True))
-            else:
-                suitable_programs = suitable_programs.order_by('-second_passing_score')
+class ProgramsCalculator(ListView):
+    model = Program
+    context_object_name = 'programs'
+    paginate_by = 30
+    template_name = 'calculator/calc_result.html'
 
-            suitable_programs = \
-                [program for program in suitable_programs.filter(custom_exam=None).all() if program.is_suitable(form_data)]
-            return render(request, 'calculator/program_list.html',
-                          {'programs': suitable_programs})
+    def get_queryset(self):
+        suitable_programs = Program.objects.prefetch_related('exams', 'university')
+        if self.request.GET['city'] != 'all':
+            suitable_programs = suitable_programs.filter(university__region_name=self.request.GET['city'])
+        if self.request.GET['sort_by'] == 'salary':
+            suitable_programs = suitable_programs.order_by(F('average_salary').desc(nulls_last=True))
+        else:
+            suitable_programs = suitable_programs.order_by('-second_passing_score')
+        if not self.request.GET.get('is_custom_exam_taken', None):
+            suitable_programs = suitable_programs.filter(custom_exam=None)
+        suitable_programs = [program for program in suitable_programs.all() if program.is_suitable(self.request.GET)]
+        return suitable_programs
 
-
-# class ProgramList(ListView, FormView):
-#
-#     template_name = 'calculator/program_detail.html'
-#     form_class = ExamsForm
-#
-#     def get_queryset(self):
-#         self.publisher = get_object_or_404(Publisher, name=self.args[0])
-#         return Book.objects.filter(publisher=self.publisher)
-#
 
 class ProgramDetailView(DetailView):
     model = Program
     context_object_name = 'program'
     template_name = 'calculator/program_detail.html'
 
+
+class UniversityDetailView(DetailView):
+    model = University
+    context_object_name = 'university'
+    template_name = 'calculator/university_detail.html'
+
 """
 # TODO 
-1. Университет детаилс
 2. Поиск?
-3. 
+3. Хранить аббревиатуры и альтернативные названия
+5. FBV to CBV & rename it
 """
