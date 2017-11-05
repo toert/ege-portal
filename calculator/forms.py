6a4f6ab2015b212895ec8a2ca449492c784ca776
toert <pw177@mail.ru>
2017-11-05 20:48:10 +0300

    Исправил ошибку в сериализации формы

--- calculator/forms.py
+++ calculator/forms.py
@@ -4,17 +4,17 @@ from universities.models import RequiredExam, Program, University
 
 
 class ExamsForm(forms.Form):
+    rus = forms.IntegerField(100, 34, label="Русский язык", required=False)
+    mat = forms.IntegerField(100, 27, label="Математика", required=False)
     soc = forms.IntegerField(100, 40, label="Обществознание", required=False)
-    geo = forms.IntegerField(100, 40, label="География", required=False)
-    lit = forms.IntegerField(100, 32, label="Литература", required=False)
     phy = forms.IntegerField(100, 36, label="Физика", required=False)
     his = forms.IntegerField(100, 29, label="История", required=False)
-    lan = forms.IntegerField(100, 22, label="Иностранный язык", required=False)
-    ict = forms.IntegerField(100, 40, label="Информатика и ИКТ", required=False, initial=100)
-    rus = forms.IntegerField(100, 34, label="Русский язык", required=False, initial=100)
-    mat = forms.IntegerField(100, 27, label="Математика", required=False, initial=100)
-    che = forms.IntegerField(100, 36, label="Химия", required=False)
     bio = forms.IntegerField(100, 36, label="Биология", required=False)
+    che = forms.IntegerField(100, 36, label="Химия", required=False)
+    lan = forms.IntegerField(100, 22, label="Иностранный язык", required=False)
+    ict = forms.IntegerField(100, 40, label="Информатика и ИКТ", required=False)
+    geo = forms.IntegerField(100, 40, label="География", required=False)
+    lit = forms.IntegerField(100, 32, label="Литература", required=False)
 
     is_custom_exam_taken = forms.BooleanField(required=False, label="Готовы сдавать внутренние испытания ВУЗов?")
     sort_by = forms.ChoiceField(choices=[('salary', 'Зарплате'), ('score', 'Проходному баллу')], label='Сортировать по',
@@ -30,3 +30,11 @@ class ExamsForm(forms.Form):
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control score'
 
+    def exams(self):
+        existed_exams = [slug for name, slug in RequiredExam.EXAMS]
+        print([field for slug, field in self.fields.items()])
+        return [field for slug, field in self.fields.items() if slug in existed_exams]
+
+    def other_fields(self):
+        existed_exams = [slug for name, slug in RequiredExam.EXAMS]
+        return filter(lambda field: field not in existed_exams, self.fields)
